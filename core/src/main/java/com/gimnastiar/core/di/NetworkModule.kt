96dcf2b43b27c1a9ce00fd6a92d89cbae766a4b9
rafli <gimnastiarrafli@gmail.com>
2024-07-27 00:03:51 +0700

    add certificate pinning at NetworkModule, ecryption at DatabaseModule

--- core/src/main/java/com/gimnastiar/core/di/NetworkModule.kt
+++ core/src/main/java/com/gimnastiar/core/di/NetworkModule.kt
@@ -19,10 +19,17 @@ class NetworkModule {
 
     @Provides
     fun provideOkHttpClient(): OkHttpClient {
+        val hostname = "api.themoviedb.org"
+        val certificatePinner = CertificatePinner.Builder()
+            .add(hostname, "sha256/5VLcahb6x4EvvFrCF2TePZulWqrLHS2jCg9Ywv6JHog=")
+            .add(hostname, "sha256/vxRon/El5KuI4vx5ey1DgmsYmRY0nDd5Cg4GfJ8S+bg=")
+            .add(hostname, "sha256/++MBgDH5WGvL9Bcn5Be30cRcL0f5O+NyoXuWtQdX1aI=")
+            .build()
         return OkHttpClient.Builder()
             .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
             .connectTimeout(120, TimeUnit.SECONDS)
             .readTimeout(120, TimeUnit.SECONDS)
+            .certificatePinner(certificatePinner)
             .build()
     }
 
